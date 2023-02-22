2e142f56edc0fa6e03e5336ba6340a32d1ca04b3
jaypersanchez <jaypersanchez@gmail.com>
2023-02-22 08:36:56 +0800

    chat response

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Button, Tabs, Tab, Container, Nav, Navbar, Form, Modal, ModalDialog } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import env from 'react-dotenv'
 import logo from './logo.svg';
 import './App.css';
 import TopBar from './components/TopBar';
 import PlayGround from './components/PlayGround'
 import AudioControl from './components/AudioControl'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [chatgptkey, setChatGptKey] = useState("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")
+  
+  useEffect(() => {
+    //console.log(`ChatGpt Key ${chatgptkey}`)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
--- src/components/PlayGround.js
+++ src/components/PlayGround.js
@@ -1,12 +1,64 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { Button, Tabs, Tab, Container, Nav, Navbar, Form, Modal } from 'react-bootstrap'
 import Web3 from 'web3';
+import axios from 'axios'
 import '../App.css'
+import TextareaAutosize from '@mui/base/TextareaAutosize';
 
 const PlayGround = () => {
+
+    const [userPrompt, setUserPrompt] = useState('');
+    const [response, setResponse] = useState('Response');
+    const [maxTokens, setMaxTokens] = useState(500)
+    const [chatgptkey, setChatGptKey] = useState("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")
+    const [gptmodel, setGptModel] = useState('text-davinci-003')
+    const [chatgpturl, setChatGptUrl] = useState(`https://api.openai.com/v1/engines/${gptmodel}/completions`)
+    const [temperature, setTemperature] = useState(1)
+    
+    useEffect(() => {
+        console.log(`ChatGpt Key ${chatgptkey}`)
+    })
+
+    const handlePrompt = async() => {
+        console.log(`${userPrompt}::${maxTokens}::${temperature}`)
+        let response = await fetch(chatgpturl, {
+            method: `POST`,
+            headers: {
+                'Content-type':'application/json',
+                'Authorization': `Bearer ${chatgptkey}`
+            },
+            body: JSON.stringify({
+                prompt: userPrompt,
+                temperature: temperature,
+                max_tokens: maxTokens
+            })
+        })
+        const data = await response.json();
+        if (data && data.choices && data.choices[0] && data.choices[0].text) {
+            console.log(data.choices[0].text)
+        }
+    }
+
     return(
         <>
         <div className='App.header'>ChatGpt Playground</div>
+        <div>
+            <Form.Group className='mb-3' id="prompt">
+                <Form.Control placeholder='Ask Your Question' onChange={(e) => {setUserPrompt(e.target.value)}}/>
+            </Form.Group>
+        </div>
+        <div>
+            <Button variant="primary" onClick={(e) => handlePrompt(e)}>Submit</Button>
+        </div>
+        <div>
+        <TextareaAutosize
+            aria-label="minimum height"
+            value={response}
+            minRows={20}
+            maxRows={50}
+            style={{ width: 1500 }}
+        />
+        </div>
         </>
     );
 }
